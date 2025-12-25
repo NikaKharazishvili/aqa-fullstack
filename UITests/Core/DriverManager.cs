d397ae4f4c4d84771fd0b43a0715e5f9e589d79f
Nika Kharazishvili <nikakharazishvili125@gmail.com>
2025-12-25 19:27:05 +0400

    minor change

--- UITests/Core/DriverManager.cs
+++ UITests/Core/DriverManager.cs
@@ -4,9 +4,9 @@ namespace UiTests.Core;
 
 public static class DriverManager
 {
-    static readonly AsyncLocal<IWebDriver?> _driver = new();
+    static readonly AsyncLocal<IWebDriver> driver = null!;
 
-    public static IWebDriver GetDriver() => _driver.Value ?? throw new InvalidOperationException("Driver not initialized! Are you in a test?");
+    public static IWebDriver GetDriver() => driver.Value ?? throw new InvalidOperationException("Driver not initialized! Are you in a test?");
 
-    public static void SetDriver(IWebDriver? driver) => _driver.Value = driver;
+    public static void SetDriver(IWebDriver given) => driver.Value = given;
 }
