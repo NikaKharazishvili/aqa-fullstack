5aedad280c112becbbcb73015cefe564433c0694
Nika Kharazishvili <nikakharazishvili125@gmail.com>
2025-12-26 13:33:17 +0400

    simplified

--- UITests/Tests/Integration/BaseTest.cs
+++ UITests/Tests/Integration/BaseTest.cs
@@ -14,52 +14,32 @@ namespace UiTests.Tests;
 public abstract class BaseTest
 {
     IWebDriver? driver;
-    string browser = ConfigReader.Get<string>("Browser").ToLowerInvariant();
-    bool headless = ConfigReader.Get<bool>("Headless");
 
     [OneTimeSetUp]
     public void Setup()
     {
+        string browser = ConfigReader.Get<string>("Browser").ToLowerInvariant();
+        bool headless = ConfigReader.Get<bool>("Headless");
 
-        driver = browser switch
+        if (browser == "chrome")
         {
-            "chrome" => CreateChrome(),
-            "firefox" => CreateFirefox(),
-            _ => throw new NotSupportedException($"Browser {browser} not supported")
-        };
+            var options = new ChromeOptions();
+            if (headless) options.AddArguments("--headless=new", "--window-size=1920,1080");
+            driver = new ChromeDriver(options);
+        }
+        else if (browser == "firefox")
+        {
+            var options = new FirefoxOptions();
+            if (headless) options.AddArguments("--headless", "--width=1920", "--height=1080");
+            driver = new FirefoxDriver(options);
+        }
+        else throw new NotSupportedException($"Browser {browser} not supported");
 
         DriverManager.SetDriver(driver);
         if (!headless) driver.Manage().Window.Maximize();
         driver.Navigate().GoToUrl(ConfigReader.Get<string>("Url"));
     }
 
-    [OneTimeTearDown]  // Quit driver after all tests are done
-    public void TearDown()
-    {
-        driver?.Quit();
-        driver?.Dispose();
-    }
-
-    IWebDriver CreateChrome()
-    {
-        var options = new ChromeOptions();
-        if (headless)
-        {
-            options.AddArgument("--headless=new");
-            options.AddArgument("--window-size=1920,1080");
-        }
-        return new ChromeDriver(options);
-    }
-
-    IWebDriver CreateFirefox()
-    {
-        var options = new FirefoxOptions();
-        if (headless)
-        {
-            options.AddArgument("--headless");
-            options.AddArgument("--width=1920");
-            options.AddArgument("--height=1080");
-        }
-        return new FirefoxDriver(options);
-    }
+    [OneTimeTearDown] // Quit driver after all tests are done
+    public void TearDown() => driver?.Dispose();
 }
